ca33b61991a47417698cbdffc4bbd701e04bc196
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2021-09-08 17:04:40 +0200

    init repo

---
+++ src/main/java/com/gokul/langapp/LangappApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.langapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gokul/langapp/LangappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.langapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
